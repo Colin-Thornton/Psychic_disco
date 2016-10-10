9c2870d7efd49f03d4a78ab9f0babab34909f38e
Colin-Thornton <spartan0366@yahoo.com>
2016-10-10 13:20:33 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/spart_000/psychic_disco/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.spart_000.psychic_disco;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
